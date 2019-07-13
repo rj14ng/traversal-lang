f34d805eb68968783c35dcaf0d716c51ccc9ec67
rj14ng <rocco.jiang5@gmail.com>
2019-07-13 20:51:24 +0800

    Update interpreter.py
    Currently this file is virtually identical to Ruslan's interpreter tutorial part 5 at https://ruslanspivak.com/lsbasi-part5/.
    
    Major code changes:
    - Add MUL and DIV tokens in order to support multiplication and division
    - Rename PLUS and MINUS tokens to ADD and SUB respectively
    - Refactor Interpreter class into Lexer and Parser classes
    - Add methods to the Parser class to implement new grammar that takes care of operator associativity and precedence
    
    The program now functions as a simple calculator that supports addition, subtraction, multiplication, and division.

--- interpreter.py
+++ interpreter.py
@@ -1,14 +1,17 @@
 # Token types
 INTEGER = 'INTEGER'
-PLUS    = 'PLUS'
-MINUS   = 'MINUS'
+ADD     = 'ADD'
+SUB     = 'SUB'
+MUL     = 'MUL'
+DIV     = 'DIV'
 EOF     = 'EOF'  # end-of-file, no more input left for lexical analysis
+
 
 class Token(object):
     def __init__(self, type, value):
-        # Token type: INTEGER, PLUS, MINUS, or EOF
+        # Token type: INTEGER, ADD, SUB, MUL, DIV, or EOF
         self.type = type
-        # Token value: non-negative integer, '+', '-', or None
+        # Token value: non-negative integer, '+', '-', '*', '/' or None
         self.value = value
     
     def __str__(self):
@@ -17,29 +20,27 @@ class Token(object):
 
         Examples:
         Token(INTEGER, 3)
-        Token(PLUS, '+')
+        Token(ADD, '+')
+        Token(MUL, '*')
         '''
         return f"Token({self.type}, {repr(self.value)})"
     
     def __repr__(self):
         return self.__str__()
 
-class Interpreter(object):
+
+class Lexer(object):
     def __init__(self, text):
-        # String input, e.g. "3 + 5" or "12 - 5"
+        # String input, e.g. "3 * 5" or "12 / 3 * 4"
         self.text = text
         # self.pos is an index into self.text
         self.pos = 0
-        # Current token instance
-        self.current_token = None
+
         self.current_char = self.text[self.pos]
     
-    ###############################################################################
-    # Lexer code                                                                  #
-    ###############################################################################
     def error(self):
         raise Exception("Error parsing input")  # might come back one day to make this error message more user friendly
-    
+
     def advance(self):
         '''
         Advance the position pointer and set the 'current_char' variable.
@@ -49,11 +50,11 @@ class Interpreter(object):
             self.current_char = None  # Indicates end of input
         else:
             self.current_char = self.text[self.pos]
-    
+
     def skip_whitespace(self):
         while self.current_char is not None and self.current_char.isspace():
             self.advance()
-    
+
     def integer(self):
         '''
         Return a (multidigit) integer consumed from the input.
@@ -81,60 +82,95 @@ class Interpreter(object):
             if self.current_char.isdigit():
                 return Token(INTEGER, self.integer())
             
-            # Return PLUS token
+            # Return ADD token
             if self.current_char == '+':
                 self.advance()
-                return Token(PLUS, '+')
+                return Token(ADD, '+')
             
-            # Return MINUS token
+            # Return SUB token
             if self.current_char == '-':
                 self.advance()
-                return Token(MINUS, '-')
+                return Token(SUB, '-')
+            
+            # Return MUL token
+            if self.current_char == '*':
+                self.advance()
+                return Token(MUL, '*')
+            
+            # Return DIV token
+            if self.current_char == '/':
+                self.advance()
+                return Token(DIV, '/')
             
             # Raise an exception if character is anything else
             self.error
         
         return Token(EOF, None)
+
+
+class Parser(object):
+    def __init__(self, lexer):
+        self.lexer = lexer
+        # Set current token to the first token taken from input
+        self.current_token = self.lexer.get_next_token()
     
-    ###############################################################################
-    # Parser code                                                                 #
-    ###############################################################################
+    def error(self):
+        raise Exception("Invalid syntax")  # might come back one day to make this error message more user friendly
+
     def eat(self, token_type):
         # Compare the current token type with the passed token type
         # If they match, 'eat' the current token then assign the next token to self.current_token
         # Otherwise raise an exception
         # This will make more sense after reading the code for the expr() function below
         if self.current_token.type == token_type:
-            self.current_token = self.get_next_token()
+            self.current_token = self.lexer.get_next_token()
         else:
             self.error()
     
-    def term(self):
+    def factor(self):
         '''
-        Return an INTEGER token value.
+        factor : INTEGER
         '''
         token = self.current_token
         self.eat(INTEGER)
         return token.value
     
+    def term(self):
+        '''
+        term : factor ((MUL | DIV) factor)*
+        '''
+        result = self.factor()
+
+        while self.current_token.type in (MUL, DIV):
+            token = self.current_token
+            if token.type == MUL:
+                self.eat(MUL)
+                result *= self.factor()
+            if token.type == DIV:
+                self.eat(DIV)
+                result /= self.factor()
+        
+        return result
+    
     def expr(self):
         '''
-        Parser / Interpreter
+        Arithmetic expression parser
 
-        expr -> INTEGER PLUS INTEGER
-        expr -> INTEGER MINUS INTEGER
+        Grammar:
+        expr   : term ((ADD | SUB) term)*
+        term   : factor ((MUL | DIV) factor)*
+        factor : INTEGER
         '''
-        # Set current token to the first token taken from the input
-        self.current_token = self.get_next_token()
 
         result = self.term()
-        while self.current_token.type in (PLUS, MINUS):
+
+        while self.current_token.type in (ADD, SUB):
             token = self.current_token
-            if token.type == PLUS:
-                self.eat(PLUS)
+            if token.type == ADD:
+                self.eat(ADD)
                 result += self.term()
-            elif token.type == MINUS:
-                self.eat(MINUS)
+            elif token.type == SUB:
+                self.eat(SUB)
                 result -= self.term()
         
         return result
@@ -147,8 +183,9 @@ def main():
             break
         if not text:
             continue
-        interpreter = Interpreter(text)
-        result = interpreter.expr()
+        lexer = Lexer(text)
+        parser = Parser(lexer)
+        result = parser.expr()
         print(result)
 
 if __name__ == "__main__":
