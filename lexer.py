cca8af1bcb1a1eea83d7eb245104014535629b00
rj14ng <rocco.jiang5@gmail.com>
2019-07-17 14:11:02 +0800

    Add synonym for OUTPUT token
    - The keywords 'output' and 'print' both result in an OUTPUT token
    - test.txt modified to check that this works

--- lexer.py
+++ lexer.py
@@ -5,15 +5,15 @@ class Lexer():
         self.lexer = LexerGenerator()
     
     def _add_tokens(self):
-        # Output
-        self.lexer.add('OUTPUT', r'output')
+        # Output/print
+        self.lexer.add('OUTPUT', r'output|print')
         # Operators
         self.lexer.add('ADD', r'\+')
         self.lexer.add('SUB', r'-')
         self.lexer.add('MUL', r'\*')
         self.lexer.add('DIV', r'/')
         # Numbers
-        self.lexer.add('FLOAT', r'\d+\.\d+')  # Check for float before integer!
+        self.lexer.add('FLOAT', r'\d+\.\d+')  # Precedence applies so check for floats before integers which would otherwise match
         self.lexer.add('INTEGER', r'\d+')
         # Parentheses
         self.lexer.add('LPAREN', r'\(')
