80d0c507dd29962d8c3cef597ab357974591baf0
rj14ng <rocco.jiang5@gmail.com>
2019-07-18 18:04:30 +0800

    Add experimental repl mode

--- main.py
+++ main.py
@@ -2,18 +2,36 @@ from lexer import Lexer
 from ast import ParserState
 from parser import Parser
 
-with open("test.txt", 'r') as test_input:
-    # State instance holds dict of variables
-    state = ParserState()
-    variables = {}
-    
-    for line in test_input.readlines():  # Reading line-by-line
-        # Lexer
-        lexer = Lexer().get_lexer()
-        tokens = lexer.lex(line)
-        
-        # Parser
-        pg = Parser()
-        pg.parse()
-        parser = pg.get_parser()
-        parser.parse(tokens, state=state).eval()
+# Parser state
+state = ParserState()
+
+# Lexer
+lexer = Lexer().get_lexer()
+
+# Parser
+pg = Parser()
+pg.parse()
+parser = pg.get_parser()
+
+mode = int(input("input file mode (1) or experimental repl mode (2)? "))
+
+if mode == 1:
+    with open("test.txt", 'r') as test_input:
+        # State instance holds dict of variables
+        state = ParserState()
+        variables = {}
+
+        for line in test_input.readlines():  # Reading line-by-line
+            tokens = lexer.lex(line)
+            parser.parse(tokens, state=state).eval()
+
+elif mode == 2:
+    while True:
+        line = input(">>> ")
+
+        try:
+            tokens = lexer.lex(line)
+            parser.parse(tokens, state=state).eval()
+
+        except Exception as e:
+            print(e)  # Probably need better error reporting in the future
